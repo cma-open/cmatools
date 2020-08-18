1ce0e4b0b0d432729c0f53c6ed757b78dce8fadb
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-08-18 17:23:30 +0100

    Updates to sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import pkg_resources
 sys.path.insert(0, os.path.abspath('../cmatools'))
 # sys.path.insert(0, os.path.abspath('../package-name'))
 
@@ -22,8 +23,11 @@ project = 'cmatools'
 copyright = '2020, jonathan winn'
 author = 'jonathan winn'
 
+# Take the version nunmber from the package version in setup
+release= pkg_resources.get_distribution("cmatools").version
+
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+#release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,8 +35,19 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
-]
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.inheritance_diagram',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.autosectionlabel',
+              'sphinx.ext.doctest',
+              'sphinx.ext.coverage',
+              'sphinx.ext.todo',
+              'sphinx.ext.viewcode',
+              #'sphinxcontrib.mermaid'   # mermaid requires pip install
+              # 'numpydoc'
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,8 +62,11 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+# A commonly used theme rtd (read the docs)
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
