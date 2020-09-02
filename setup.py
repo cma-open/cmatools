23e468acf06d2557fa0d8679b5f9d23c4e26cec3
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-02 15:36:15 +0100

    Updates console scripts and code comments

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 
+# Use text from the main repo readme for the package long description
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -12,6 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cma-open/cmatools",
+    # find and install all packages
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,5 +22,15 @@ setuptools.setup(
     ],
     python_requires='>=3.7',
     # include_package_data = True,
-    # entry_points
+
+    # register command line scripts from the relevant package module
+    entry_points={
+        'console_scripts': [
+            'cli-simple=cmatools.cli_simple:'
+            'cli_simple_entry_point',
+
+            'cli-analysis=cmatools.cli_analysis:'
+            'cli_analysis_entry_point',
+                        ]
+    }
 )
