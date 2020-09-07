2ab28a6a62f0f0a4c0f4316f4f5e541248cde695
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-07 11:26:22 +0100

    Refactor code style, whitepsace edits

--- cmatools/definitions.py
+++ cmatools/definitions.py
@@ -1,6 +1,7 @@
 """
 .. module: definitions
-Source definitions file to generate constants
+
+Source definitions file to generate constants.
 Currently used to generate main code source root path
 for use by other modules
 
@@ -8,5 +9,6 @@ for use by other modules
 
 import os
 
-""" The root directory for CMATOOLS package"""
+""" The root directory for CMATOOLS package """
+
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
