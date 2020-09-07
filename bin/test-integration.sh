d83a021299926b8a25df409c404e6f4f58eec564
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-07 11:24:29 +0100

    Adds and updates tests

--- bin/test-integration.sh
+++ bin/test-integration.sh
@@ -6,4 +6,6 @@ echo "$testdir"
 # Discover and run tests on code path,  -v verbose flag
 #pytest -r -l --tb=long  $testdir
 #pytest --tb=long  $testdir
-pytest -rA  $testdir/tests/integration
+
+
+pytest -rAx  $testdir/tests/integration
