a6dfb6616d5ae3cf606875b729f1dd078d067645
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-18 11:30:08 +0100

    Adds source config reader

---
+++ cmatools/io/read_source.py
@@ -0,0 +1,31 @@
+import configparser
+from cmatools.definitions import ROOT_DIR
+
+config = configparser.ConfigParser()
+
+configfile = f"{ROOT_DIR}/cmatools/config.ini"
+
+class SourceData():
+
+    def __init__(self, service):
+        self.service=service
+        self.dataset=None
+        self.format=None
+        #self.format=self.read_input_source_ini(service)
+
+
+    def read_input_source_ini(self):
+        """Read input sources from the ini file"""
+
+        config.read(configfile)
+
+        if self.service is 'COP':
+
+            self.dataset = config.get("SOURCES", "COP_DATASET")
+            self.format = config.get("SOURCES", "COP_FORMAT")
+            product_type = config.get("SOURCES", "COP_PRODUCT_TYPE")
+            variables = config.get("SOURCES", "COP_VARIABLES")
+            grid_res = config.get(("SOURCES"), "COP_GRID_RES")
+            period = config.get("SOURCES", "COP_PERIOD")
+            version = config.get("SOURCES", "COP_VERSION")
+
