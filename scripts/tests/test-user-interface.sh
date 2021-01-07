77c0dbfac0bc18477f8b66ec019617c598e0c8b6
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-01-07 20:14:58 +0000

    Adds script to run user interface tests

---
+++ scripts/tests/test-user-interface.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#######################################
+# Script to run system tests
+# Runs the user interface tests
+#######################################
+
+# Set python package root dir as script constant
+readonly CODE_DIR="$(dirname "$(dirname "${PWD}")")"
+# Set tests directory
+readonly TESTS_DIR="${CODE_DIR}"/tests
+
+echo " ---- * ----"
+echo "Running system tests with pytest"
+echo "Python package root: "${CODE_DIR}""
+echo "Tests directory: "${TESTS_DIR}""
+echo " ---- * ----"
+
+# Discover and run tests on code path,
+# -v verbose flag
+# -r displays “short test summary info” at end of session, -A lists all info
+
+#pytest -r -l --tb=long  $testdir
+
+# Run pytests on the user interface tests subdirectory
+pytest -rA  "${TESTS_DIR}"/unser-interface
+
