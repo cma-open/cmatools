21574b5b45661cf24ab08c6e98fb160f6d917881
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-02-09 16:07:50 +0000

    Amend structure and style, update code comments

--- tests/unit/version/test_version.py
+++ tests/unit/version/test_version.py
@@ -1,27 +1,20 @@
-""" Test the package version
-"""
+""" Test the package version """
 
-#from importlib.metadata import version
-#version('construct')
-#'4.3.1'
+# from importlib.metadata import version
+# version('cmatools')
+
 # TODO update for python 3.8
 
 import pkg_resources
-#pkg_resources.get_distribution('cmatools').version
 
-from cmatools.definitions import ROOT_DIR
-
-
+# from cmatools.definitions import ROOT_DIR
 
 def test_package_version():
 
     """Test for current package version"""
 
-    version = pkg_resources.get_distribution('cmatools').version
+    version = pkg_resources.get_distribution("cmatools").version
     print(version)
 
     # Confirm version is as expected
-    #assert isinstance(out, argparse.ArgumentParser)
-
-
-
+    # assert isinstance(out, argparse.ArgumentParser)
