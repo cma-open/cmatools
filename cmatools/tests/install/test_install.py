f580303e3fb2c6b44fa2e75f9523e372162dd952
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-02-16 22:16:26 +0000

    Updates test to show location of installed package

--- cmatools/tests/install/test_install.py
+++ cmatools/tests/install/test_install.py
@@ -1,15 +1,17 @@
 """ Test and confirm how the package is installed """
 
 import pkg_resources
+import subprocess
 
 from cmatools.definitions import ROOT_DIR
 
+PACKAGE="cmatools"
 
 def test_package_installed():
 
     """Test for current package installation location"""
 
-    version = pkg_resources.get_distribution("cmatools").version
+    version = pkg_resources.get_distribution(PACKAGE).version
     print(version)
-
+    subprocess.run(["pip", "show", PACKAGE], check=True)
 
