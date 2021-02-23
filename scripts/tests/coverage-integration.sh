abaca662984765dd9d8ab503d013a89124f7edcf
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-02-23 20:37:09 +0000

    Updates and adds further code coverage test scripts, by test type

--- scripts/tests/coverage-integration.sh
+++ scripts/tests/coverage-integration.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
 ######################################################################################################################
-# Script to system unit tests
+# Script to test: integration tests
 ######################################################################################################################
 
-# Set python package root dir as script constant
-readonly CODE_DIR="$(dirname "$(dirname "${PWD}")")"
-# Set tests directory
-readonly TESTS_DIR="${CODE_DIR}"/tests/unit
+# Source code, variables from common.sh
+source common.sh
 
-echo " ---- * ----"
-echo "Running system tests with pytest"
-echo "Python package root: "${CODE_DIR}""
-echo "Tests directory: "${TESTS_DIR}""
-echo " ---- * ----"
-
-# pytest --tb=long -vrA  "${TESTS_DIR}"
-
-cd "${CODE_DIR}"
+echo "Current working directory: ${PWD}"
 
 # Discover and run tests on specified path, with coverage stats
-pytest --cov-report term-missing:skip-covered --cov="${CODE_DIR}"/cmatools "${TESTS_DIR}"
-echo " ---- "
-echo "  # End of unit tests"
-echo " ---- "
+pytest --cov-report term-missing:skip-covered --cov="${PACKAGE}" "${TESTS_DIR}/integration"
 
 
 ######################################################################################################################
