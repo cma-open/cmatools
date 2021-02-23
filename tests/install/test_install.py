a579962062981db3c0b23a3554203eb8a4e71b69
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-02-23 20:39:04 +0000

    Moves test directory back to project root, makes tests a package, and subpackages

--- tests/install/test_install.py
+++ tests/install/test_install.py
@@ -12,6 +12,13 @@ def test_package_installed():
     """Test for current package installation location"""
 
     version = pkg_resources.get_distribution(PACKAGE).version
-    print(version)
+    print("Version: ", version)
     subprocess.run(["pip", "show", PACKAGE], check=True)
+    print("Source code root: ", ROOT_DIR)
 
+def test_installed_version():
+    """Test for current package version"""
+
+    version = pkg_resources.get_distribution(PACKAGE).version
+    assert version == "0.0.1"
+    # TODO - replace this tests with a call to compare source version from files vs current installed version
