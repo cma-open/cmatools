68fe1a5c20faef1fa2f9c70a8b889d89210e1da8
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-03-02 10:59:19 +0000

    Updates code comments

--- src/cmatools/definitions.py
+++ src/cmatools/definitions.py
@@ -2,17 +2,16 @@
 .. module: definitions
 
 Source definitions file to generate constants.
-Currently used to generate main code source root path
+Currently used to generate main code source root path and package name
 for use by other modules
 
 """
 
 import os
 
-""" Constants for the package """
-
+# Set the root dir relative to this file
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Set the package name
-# TODO check r.e. takename from setup.py
+# TODO check r.e. take name from setup.py to minimise duplication
 PACKAGE = "cmatools"
