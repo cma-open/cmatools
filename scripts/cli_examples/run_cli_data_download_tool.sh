6abdb5ce35fabeec8231cedb6e7ea9ce14ce3598
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-08 21:19:11 +0000

    Example use files calling cli commands

---
+++ scripts/cli_examples/run_cli_data_download_tool.sh
@@ -0,0 +1,21 @@
+#  Example use of the cli-data-downlaod command line tool - with user arguments
+
+# Usage
+# Call the command by name, supplies user specified arguments
+
+cli-data-download --x=1 --y=2 --portal=HADOBS --dataset=CRUTEM
+
+echo " --- "
+echo ""
+
+cli-data-download --x=1 --y=2 --portal=CEDA --dataset=CRUTEM
+
+echo " --- "
+echo ""
+
+cli-data-download --x=1 --y=2 --portal=HADOBS --dataset=HADCRUT
+
+echo " --- "
+echo ""
+
+cli-data-download --x=1 --y=2 --portal=CEDA --dataset=HADCRUT
