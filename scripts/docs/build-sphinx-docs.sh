9e48b08f5c20583a74215751faabe3817cdea3eb
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-08 21:34:04 +0000

    Adds script to build sphinx documentation from source files

---
+++ scripts/docs/build-sphinx-docs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+######################################################################################################################
+# Script to build system documentation
+# Outputs files to /docs/build
+######################################################################################################################
+
+# Set python package root dir as script constant
+readonly CODE_DIR="$(dirname "$(dirname "${PWD}")")"
+# Source variables from common
+source "${CODE_DIR}/scripts/common/common.sh"
+
+# Set documentation directories
+readonly DOCS_DIR="${CODE_DIR}/docs"
+readonly INPUT_DIR="${CODE_DIR}/docs/source"
+readonly OUTPUT_DIR="${CODE_DIR}/docs/build"
+# Set named docs subdirectories
+readonly API_DIR="${CODE_DIR}/docs/source/api"
+readonly TEMPLATE_DIR="${CODE_DIR}/docs/source/templates"
+
+echo " ---- * ----"
+echo "Creating Sphinx system documentation"
+echo "Python package root: ${CODE_DIR}"
+echo "Sphinx source doc files: ${INPUT_DIR}"
+echo "Sphinx output build files: ${OUTPUT_DIR}"
+echo "Sphinx API dir: ${API_DIR}"
+echo "Sphinx template dir: ${TEMPLATE_DIR}"
+echo " ---- * ----"
+
+# Generate source files from current installed package / subpackages, sphinx-apidoc is a tool for automatic
+# Generation of Sphinx sources using the autodoc extension documents a whole package
+# Templates and options allow customisation of output doc pages
+
+# sphinx-apidoc -f  --module-first --no-headings --templatedir=$TEMPLATE_DIR -o $API_DIR $CODE_DIR/cmatools
+#sphinx-apidoc -f  --module-first --templatedir="${TEMPLATE_DIR}" -o "${API_DIR}" "${CODE_DIR}/src/${PACKAGE}"
+# this writes new source rst files to the dir set by -o
+
+# Build sphinx docs and make html files
+sphinx-build -v -b html "${INPUT_DIR}" "${OUTPUT_DIR}"
