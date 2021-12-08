8960a3373f237bd8a2ca3ebf5dafc4e996636f12
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-08 21:08:30 +0000

    Move to source layout, amend Sphinx settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+"""Sphinx config settings."""
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -9,25 +11,28 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 import os
 import sys
-import pkg_resources
-sys.path.insert(0, os.path.abspath('../cmatools'))
-# sys.path.insert(0, os.path.abspath('../package-name'))
 
+import pkg_resources
+
+sys.path.insert(
+    0, os.path.abspath('../src/cmatools')
+)  # Note level within src directory
+# sys.path.insert(0, os.path.abspath('../package-name'))
 
 # -- Project information -----------------------------------------------------
 
 project = 'cmatools'
-copyright = '2020, jonathan winn'
-author = 'jonathan winn'
+copyright = '2021, Jonathan Winn'
+author = 'Jonathan Winn'
 
 # Take the version nunmber from the package version in setup
-release= pkg_resources.get_distribution("cmatools").version
+release = pkg_resources.get_distribution('cmatools').version
 
 # The full version, including alpha/beta/rc tags
-#release = '0.0.1'
+# release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -35,23 +40,26 @@ release= pkg_resources.get_distribution("cmatools").version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.githubpages',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.inheritance_diagram',
-              'sphinx.ext.autosummary',
-              'sphinx.ext.autosectionlabel',
-              'sphinx.ext.doctest',
-              'sphinx.ext.coverage',
-              'sphinx.ext.todo',
-              'sphinx.ext.viewcode',
-              'sphinxarg.ext',
-              'sphinxcontrib.mermaid'   # mermaid requires pip install
-              # 'numpydoc'
-              ]
+extensions = [
+    'sphinx.ext.autodoc',
+    # 'sphinx_autodoc_typehints',  test this later # TODO
+    'sphinx.ext.githubpages',
+    # 'sphinx.ext.napoleon',
+    'sphinx.ext.inheritance_diagram',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinxarg.ext',
+    'sphinxcontrib.mermaid',
+    'numpydoc'
+    # 'alabaster'
+]
 # Set docstring style options
-napoleon_google_docstring = False
-napoleon_numpy_docstring = True
+# napoleon_google_docstring = False
+# napoleon_numpy_docstring = True
 
 # Allow todo panels to be highlighted in outputs docs
 # Relates to the sphinx.ext.todo extension
@@ -65,18 +73,58 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
 #
-#html_theme = 'alabaster'
+# html_theme = 'alabaster' # tested but decided againt
 # A commonly used theme rtd (read the docs)
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
+# html_theme = "pydata_sphinx_theme"
+# html_theme="sphinxdoc"
+# html_theme="sphinx13"
+
+numpydoc_attributes_as_param_list = False  # include attributes in a table
+numpydoc_xref_param_type = True
+
+numpydoc_xref_aliases = {
+    'LeaveOneOut': 'sklearn.model_selection.LeaveOneOut',
+}
+# TODO add more custom refs here
+
+# numpydoc_validation_checks = {"all"}
+# TODO enable later
+
+autosummary_generate = True
+# autosummary_imported_members = True
+autodoc_member_order = 'groupwise'
+autodoc_default_flags = ['show-inheritance']
+# autodoc_docstring_signature = True
+autoclass_content = 'both'  # Add __init__ doc (ie. params) to class summaries
+
+# test later # TODo
+add_module_names = False  # Remove namespaces from class/method signatures
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# copy to decisioon log
+# decided numpydoc over napoloeon
+# https://numpydoc.readthedocs.io/en/latest/install.html
+# TODO check order of class methods and attributes, seem smixed up
+# TODO - need to see how other config options impact output format
+
+# Configuration for intersphinx
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/devdocs/', None),
+    'sklearn': ('https://scikit-learn.org/stable/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
+}
+
+# Refs
+# https://stackoverflow.com/questions/2701998/sphinx-autodoc-is-not-automatic-enough
