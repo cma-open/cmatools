1b14e5f3db57916741f27874c3003f1b094ee087
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-13 20:30:34 +0000

    Adds tests of installation

---
+++ tests/d_user-interface/install/test_install.py
@@ -0,0 +1,26 @@
+"""Test and confirm how the package is installed."""
+
+import subprocess
+from importlib import metadata
+
+import pkg_resources
+
+from cmatools.definitions import PACKAGE, ROOT_DIR
+
+
+def test_package_installed():
+    """Test current package installation location."""
+    version = pkg_resources.get_distribution(PACKAGE).version
+    print('Version: ', version)
+    subprocess.run(['pip', 'show', PACKAGE], check=True)
+    print('Source code root: ', ROOT_DIR)
+
+
+def test_installed_version():
+    """Test for current package version."""
+    version = pkg_resources.get_distribution(PACKAGE).version
+    assert version == '0.0.1'
+    # TODO - replace this test with a call to compare source
+    #  version from files vs current installed version
+    version_alternative = metadata.version(PACKAGE)
+    assert version == version_alternative
