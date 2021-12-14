8bb4937a9debbed9daae8bd57af4a61ccfd167b6
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-14 10:51:10 +0000

    Adds test using mock

---
+++ tests/a_unit/combine/test_combined.py
@@ -0,0 +1,19 @@
+"""Test combined function."""
+
+from mock import patch
+
+from cmatools.combine.combine import combined
+
+
+@patch('cmatools.combine.combine.analysis_product')
+@patch('cmatools.combine.combine.analysis_sum')
+def test_combined(mock_sum, mock_product):
+    """Test of combined function."""
+    mock_sum.return_value = 4
+    mock_product.return_value = 4
+    # Call combined function. Note the arg values are ignored because
+    # the functions within combined have been mocked
+    result = combined(2, 3)
+    # analysis_product will return 4, analysis_sum will return 4
+    expected = 8  # 8 = the sum of mocks ( 4 + 4)
+    assert result == expected
