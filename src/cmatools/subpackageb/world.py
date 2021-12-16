4516b9d4de1d9c0ceead57f7d1abaf306de8e7f8
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-16 12:19:52 +0000

    Adds example subpackage module

---
+++ src/cmatools/subpackageb/world.py
@@ -0,0 +1,11 @@
+"""world module with two example functions."""
+
+
+def world():
+    """World."""
+    return 'cma data'
+
+
+def all_world():
+    """All world scope."""
+    return 'all world coverage'
