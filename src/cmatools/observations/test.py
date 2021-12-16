04580defd86e6ccc27929671994219e8dba5c03f
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-16 13:04:12 +0000

    Adds test module

---
+++ src/cmatools/observations/test.py
@@ -0,0 +1,19 @@
+"""Test module."""
+
+
+def this_one() -> str:
+    """Print this.
+
+    Returns
+    -------
+    str
+        String 'this'
+
+    """
+    print('this')
+    return 'this'
+
+
+def another_one():
+    """Print that."""
+    print('that')
