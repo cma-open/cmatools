643e1cfc9b337a10c8db51b0800cd84809908540
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-20 17:54:55 +0000

    Adds docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,24 @@
-# Added to catch situation where tests are run before the package has been installed
+"""Configuration for pytest."""
+
+# Added to catch situations where tests are run before the package has been installed
 
 import pytest
 
-def pytest_collectreport(report):
 
+def pytest_report_header(config):
+    """Add additional text to output reports."""
+    return 'Extra info: example'
+
+
+def pytest_collectreport(report):
+    """Report message to user if test collection fails."""
     if report.failed:
 
         raise pytest.UsageError(
-
-            "Errors during collection \n"
-            "Check package has been installed correctly \n"
-            "Have you run 'python setup.py install' or 'pip install .'  ? \n"
-            "Aborting tests"
+            '- \n'
+            'Errors during collection \n'
+            'Check package has been installed correctly \n'
+            "Have you run 'pip install .' or 'pip install -e .' ? \n"
+            'Aborting tests'
+            ' \n'
         )
