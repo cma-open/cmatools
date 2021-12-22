b3fcdceb77c41736f7def93b713b0bd3fe54037a
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2021-12-22 12:58:48 +0000

    Updates to delete autosummary

--- scripts/docs/delete-build-dir.sh
+++ scripts/docs/delete-build-dir.sh
@@ -11,12 +11,15 @@ source "${CODE_DIR}/scripts/common/common.sh"
 
 # Set documentation directories
 readonly OUTPUT_DIR="${CODE_DIR}/docs/build"
+readonly AUTOSUMMARY="${CODE_DIR}/docs/source/_autosummary"
 
 echo " ---- * ----"
 echo "Deleting Sphinx build directory"
 echo "Python package root: ${CODE_DIR}"
 echo "Sphinx output build files: ${OUTPUT_DIR}"
+echo "Sphinx autosummary source files: ${AUTOSUMMARY}"
 echo " ---- * ----"
 
 # Delete sphinx docs build
 rm -rf "${OUTPUT_DIR}"
+rm -rf "${AUTOSUMMARY}"
