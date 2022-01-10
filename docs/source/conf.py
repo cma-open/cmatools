3d55cea2af9ae1b577e4b4861610fc4f2d051dda
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2022-01-10 19:01:17 +0000

    Fix codestyle

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,48 +17,49 @@ import pkg_resources
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 # sys.path.insert(0, os.path.abspath('../../src'))
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 
 
-print('------------------')
-print('System path:')
-print(*sys.path, sep='\n')
-print('------------------')
+print("------------------")
+print("System path:")
+print(*sys.path, sep="\n")
+print("------------------")
 
 # tests is not installed by setup, therefore import here to ensure available
 # by relative import path
 import tests  # noqa: F401, E402 isort:skip
+
 # import cmatools  # noqa: F401, E402 isort:skip
 
 # -- Project information -----------------------------------------------------
 
-project = 'cmatools'
-copyright = '2021, Jonathan Winn'
-author = 'Jonathan Winn'
+project = "cmatools"
+copyright = "2021, Jonathan Winn"
+author = "Jonathan Winn"
 # Take the version nunmber from the package version in setup
-release = pkg_resources.get_distribution('cmatools').version
+release = pkg_resources.get_distribution("cmatools").version
 
 # -- General configuration ---------------------------------------------------
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
     # 'sphinx_autodoc_typehints',  test this later # TODO
-    'sphinx.ext.githubpages',
+    "sphinx.ext.githubpages",
     # 'sphinx.ext.napoleon',
-    'sphinx.ext.inheritance_diagram',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.doctest',
-    'sphinx.ext.coverage',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'sphinxarg.ext',
-    'sphinxcontrib.mermaid',
-    'numpydoc'
+    "sphinx.ext.inheritance_diagram",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinxarg.ext",
+    "sphinxcontrib.mermaid",
+    "numpydoc",
 ]
 
 # Allow todo panels to be highlighted in outputs docs
@@ -66,7 +67,7 @@ extensions = [
 todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -78,13 +79,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.
 # See documentation for a list of builtin themes.
 # A commonly used theme rtd (read the docs)
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 # html_theme = 'alabaster' # tested but decided against
 
 numpydoc_attributes_as_param_list = False  # include attributes in a table
 numpydoc_xref_param_type = True
 numpydoc_xref_aliases = {
-    'LeaveOneOut': 'sklearn.model_selection.LeaveOneOut',
+    "LeaveOneOut": "sklearn.model_selection.LeaveOneOut",
 }
 # TODO add more custom refs here
 
@@ -93,10 +94,10 @@ numpydoc_xref_aliases = {
 
 autosummary_generate = True
 # autosummary_imported_members = True
-autodoc_member_order = 'groupwise'
-autodoc_default_flags = ['show-inheritance']
+autodoc_member_order = "groupwise"
+autodoc_default_flags = ["show-inheritance"]
 # autodoc_docstring_signature = True
-autoclass_content = 'both'  # Add __init__ doc (ie. params) to class summaries
+autoclass_content = "both"  # Add __init__ doc (ie. params) to class summaries
 
 # Allow ref links to section titles by page/section ref
 autosectionlabel_prefix_document = True
@@ -107,7 +108,7 @@ add_module_names = False  # Remove namespaces from class/method signatures
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # copy to decision log
 # decided numpydoc over napoloeon
@@ -117,12 +118,12 @@ html_static_path = ['_static']
 
 # Configuration for intersphinx
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'numpydoc': ('https://numpydoc.readthedocs.io/en/latest/', None),
-    'sklearn': ('https://scikit-learn.org/stable/', None),
-    'matplotlib': ('https://matplotlib.org/stable/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "numpydoc": ("https://numpydoc.readthedocs.io/en/latest/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 
 # Refs
--- setup.py
+++ setup.py
@@ -7,70 +7,68 @@ import setuptools
 # TODO add log or better user warning re use of datadir and home directory
 
 warnings.warn(
-    'Warning: once installed and used this software will write to '
-    'locations in the home directory'
+    "Warning: once installed and used this software will write to "
+    "locations in the home directory"
 )
 warnings.warn(
-    'Warning: edit the config.ini to use project specific directory '
-    'names, if required'
+    "Warning: edit the config.ini to use project specific directory "
+    "names, if required"
 )
-warnings.warn('Info: see user documentation for more details')
+warnings.warn("Info: see user documentation for more details")
 
 # requires pip install . -v
 # or python setup.py install
 
 
 # Use text from the main repo readme for the package long description
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='cmatools',
-    version='0.0.1',
-    author='Jonathan Winn',
-    author_email='jonathan.winn@metoffice.com',
-    description='Training example package',
+    name="cmatools",
+    version="0.0.1",
+    author="Jonathan Winn",
+    author_email="jonathan.winn@metoffice.com",
+    description="Training example package",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/cma-open/cmatools',
+    long_description_content_type="text/markdown",
+    url="https://github.com/cma-open/cmatools",
     # find and install all packages
-    package_dir={'': 'src'},
+    package_dir={"": "src"},
     # Legacy / Maintenance note:
     # As the package dir is  specified, then don't need to also exclude the tests here
     # However retained as a failsafe in case future tests are added in the main package
-    packages=setuptools.find_packages(where='src', exclude=['*tests.*', '*tests']),
-    license='BSD',
+    packages=setuptools.find_packages(where="src", exclude=["*tests.*", "*tests"]),
+    license="BSD",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
     ],
     # Set minimum python version to allow installation
-    python_requires='>=3.9',
+    python_requires=">=3.9",
     # Set key dependency versions required to allow installation
     install_requires=[
-        'scitools-iris>=3.0',  # Note alt name for iris via pip, c.f. conda-forge
-        'numpy>=1.19',
+        "scitools-iris>=3.0",  # Note alt name for iris via pip, c.f. conda-forge
+        "numpy>=1.19",
     ],
     # Include data files, as listed in MANIFEST.in (e.g. config.ini)
     include_package_data=True,
-
     # Register command line scripts from the relevant package module
     # These are added as command line options once the system is installed
     entry_points={
         # Name the tool, link to the package function
-        'console_scripts': [
+        "console_scripts": [
             # Name the simple analysis command
-            'cli-simple-analysis=' 'cmatools.cli_simple_analysis:cli_entry_point',
+            "cli-simple-analysis=" "cmatools.cli_simple_analysis:cli_entry_point",
             # Set the data download command
-            'cli-data-download='
-            'cmatools.cli_data_download:cli_data_download_entry_point',
+            "cli-data-download="
+            "cmatools.cli_data_download:cli_data_download_entry_point",
             # Set the copernicus data download command
-            'cli-copernicus-download='
-            'cmatools.cli_copernicus_download:cli_copernicus_download_entry_point',
+            "cli-copernicus-download="
+            "cmatools.cli_copernicus_download:cli_copernicus_download_entry_point",
             # Set function to write out current config to file
-            'write-config='
-            'cmatools.common.write_config:write_config',
+            "write-config=" "cmatools.common.write_config:write_config",
         ]
     },
 )
