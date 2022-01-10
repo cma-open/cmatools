e5c7e9348fc2ecb8530a56febdf979efea63615d
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2022-01-10 18:37:14 +0000

    Fix codestyle

--- src/cmatools/common/write_config.py
+++ src/cmatools/common/write_config.py
@@ -10,12 +10,12 @@ from cmatools.io.io_common import return_datadir_root_dir
 # Read from user-editable config file
 config = configparser.ConfigParser()
 config.read(CONFIGFILE)
-app_env = config.get('APP', 'ENVIRONMENT')
-datadir_root = config.get('DATADIR', 'ROOT')
-datadir_inputs = config.get('DATADIR', 'INPUTS')
-datadir_archives = config.get('DATADIR', 'ARCHIVES')
-logs_config = config.get('LOGS', 'CONFIG')
-scratchdir_root = config.get('SCRATCHDIR', 'ROOT')
+app_env = config.get("APP", "ENVIRONMENT")
+datadir_root = config.get("DATADIR", "ROOT")
+datadir_inputs = config.get("DATADIR", "INPUTS")
+datadir_archives = config.get("DATADIR", "ARCHIVES")
+logs_config = config.get("LOGS", "CONFIG")
+scratchdir_root = config.get("SCRATCHDIR", "ROOT")
 
 DEBUG = True
 """bool: Debugging level, module constant (Default: True)."""
@@ -25,16 +25,17 @@ def write_config():
     """Write elements from config file to disk."""
     log = Path(ROOT_DIR) / logs_config
     if DEBUG:
-        print(f'Log: {log}')
+        print(f"Log: {log}")
     # TODO expand functionality
     # Just POC, to show simple write out to log file
-    with open(log, 'w') as filehandle:
+    with open(log, "w") as filehandle:
         filehandle.write(f'Log time: {datetime.now().strftime("%Y-%m-%d %H:%M")}\n')
-        filehandle.write(f'App dev stage: {app_env}\n')
-        filehandle.write(f'DataDir root: {datadir_root}\n')
-        filehandle.write(f'DataDir root full: '
-                         f'{return_datadir_root_dir(datadir_root)}\n')
-        filehandle.write(f'Inputs: {datadir_inputs}\n')
-        filehandle.write(f'ScratchDir root: {scratchdir_root}\n')
+        filehandle.write(f"App dev stage: {app_env}\n")
+        filehandle.write(f"DataDir root: {datadir_root}\n")
+        filehandle.write(
+            f"DataDir root full: " f"{return_datadir_root_dir(datadir_root)}\n"
+        )
+        filehandle.write(f"Inputs: {datadir_inputs}\n")
+        filehandle.write(f"ScratchDir root: {scratchdir_root}\n")
         if DEBUG:
-            filehandle.write(f'Archive: {datadir_archives}\n')
+            filehandle.write(f"Archive: {datadir_archives}\n")
