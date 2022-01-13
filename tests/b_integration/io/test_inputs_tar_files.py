f3a2537bf5effbf32ce228758568b1f67593a189
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2022-01-13 18:25:16 +0000

    Adds canned data for tests and dev use

--- tests/b_integration/io/test_inputs_tar_files.py
+++ tests/b_integration/io/test_inputs_tar_files.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import iris
 
-from cmatools.io.io_common import return_datadir_inputs_dir
+from cmatools.definitions import ROOT_DIR
 
 # TODO - refactor
 # TODO add code to deal with file does not exist yet
@@ -13,21 +13,22 @@ from cmatools.io.io_common import return_datadir_inputs_dir
 # change to project ini level value
 DEBUG = True
 
-filename = "eobs.tgz"
-outputfilepath = Path(return_datadir_inputs_dir()) / filename
+filename = "hadcrut_example.tar.gz"
+canned_data = f"{ROOT_DIR}/tests/_data"
+test_data_file = Path(canned_data) / filename
 
 
 def test_tarfile_read_cubes(tmp_path):
     """Test read in netcdf via iris."""
-    with tarfile.open(outputfilepath, "r") as archive:
+    with tarfile.open(test_data_file, "r") as archive:
         print(f"Archive members: {archive.getmembers()}")
         print(f"Archive names: {archive.getnames()}")
         # For situations where there is only 1 file in the archive
         content_filename = archive.getnames()[0]
         print(f"Content filename: {content_filename}")
         archive.extract(content_filename, path=tmp_path)
+        # iris does not deal with pathlib well yet, convert to string
         cubes = iris.load(str(tmp_path / content_filename))
-        # iris does not deal with pathlib well yet
         print(cubes)
         print(f"Cube type for cubes[0]: {type(cubes[0])}")
         assert isinstance(cubes[0], iris.cube.Cube)
